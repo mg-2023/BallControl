683bdcad3e9741fbd4b83229a08dafc7d7a4db74
mg-2018 <alsrl6710@gmail.com>
2021-02-14 13:48:13 +0900

    v 0.2a
    - add to stage6 (stage6 is incomplete)
    - add dash item sprite
    TODO: include all stages into one scene

---
+++ Assets/Scripts/ItemDash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDash : MonoBehaviour
+{
+	BoxCollider2D bc;
+	
+	public GameObject ball;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		bc = gameObject.AddComponent<BoxCollider2D>();
+	}
+	
+	void OnColliderEnter2D()
+	{
+		ball.SpriteRenderer.color = new Color(0f, 0f, 0f, 1f);
+		
+		Destroy(gameObject);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
